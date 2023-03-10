365e363b88a07cd1f0076c65c9c325986f10cc8c
pecheylauren02 <pechey.lauren02@gmail.com>
2023-03-10 09:09:16 +0000

    Create new js file to hold questions

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -3,146 +3,8 @@ let progressBar = document.getElementById("progress-bar");
 let progressText = document.getElementById("progress-text");
 let input = document.getElementById("name-submit");
 let takeQuizButton = document.getElementById("take-quiz-btn");
-document.getElementById("footer-area").classList.remove("hide");
-
-
-//let currentQuestion = document.getElementById("current-question");
-
-const QUESTIONS = [{
-        'question': "Which superpower appeals to you the most?",
-        "optionOne": "The ability to fly",
-        "optionTwo": "Being invisible",
-        "optionThree": "Controlling the weather",
-        "optionFour": "Super strength",
-        "answers": [
-            ["The ability to fly", 4],
-            ["Being invisible", 2],
-            ["Controlling the weather", 1],
-            ["Super strength", 3]
-        ]
-    },
-    {
-        'question': "What made you become a villain?",
-        "optionOne": "I want justice",
-        "optionTwo": "My parents were killed",
-        "optionThree": "I was bullied at school",
-        "optionFour": "Loneliness",
-        "answers": [
-            ["I want justice", 1],
-            ["My parents were killed", 3],
-            ["I was bullied at school", 4],
-            ["Loneliness", 2]
-        ]
-
-    },
-    {
-        'question': "What is your deepest desire as a villain?",
-        "optionOne": "To destroy anything that comes in my way",
-        "optionTwo": "To start a revolution against society",
-        "optionThree": "To get revenge on my nemesis",
-        "optionFour": "To take over the world",
-        "answers": [
-            ["To destroy anything that comes in my way", 3],
-            ["To start a revolution against society", 2],
-            ["To get revenge on my nemesis", 4],
-            ["To take over the world", 1]
-        ]
-
-    },
-    {
-        'question': "Which animal would you choose as your sidekick?",
-        "optionOne": "A python",
-        "optionTwo": "A rottweiler",
-        "optionThree": "A raven",
-        "optionFour": "A giant tarantula",
-        "answers": [
-            ["A python", 1],
-            ["A rottweiler", 3],
-            ["A raven", 2],
-            ["A giant tarantula", 4]
-        ]
-    },
-    {
-        'question': "What is your biggest weakness?",
-        "optionOne": "My fear of being left alone",
-        "optionTwo": "My traumatic childhood",
-        "optionThree": "The love of my life",
-        "optionFour": "I will never reveal my weakness",
-        "answers": [
-            ["My fear of being left alone", 2],
-            ["My traumatic childhood", 4],
-            ["The love of my life", 3],
-            ["I will never reveal my weakness", 1]
-        ]
-    },
-    {
-        'question': "Where is your secret hideout?",
-        "optionOne": "Inside a cave",
-        "optionTwo": "In an abandoned house",
-        "optionThree": "In a luxurious mansion",
-        "optionFour": "Deep in the ocean",
-        "answers": [
-            ["Inside a cave", 2],
-            ["In an abandoned house", 4],
-            ["In a luxurious mansion", 1],
-            ["Deep in the ocean", 3]
-        ]
-    },
-    {
-        'question': "Your best friend betrays you. How do you deal with them?",
-        "optionOne": "Wallow in your own self-pity",
-        "optionTwo": "Destroy their reputation",
-        "optionThree": "Act normal but secretly plot your revenge",
-        "optionFour": "Go solo, you don't need anyone else",
-        "answers": [
-            ["Wallow in your own self-pity", 2],
-            ["Destroy their reputation", 3],
-            ["Act normal but secretly plot your revenge", 4],
-            ["Go solo, you don't need anyone else", 1]
-        ]
-    },
-    {
-        'question': "Which weapons would you use against your enemies?",
-        "optionOne": "Magic potions and spells",
-        "optionTwo": "My own brute force and strength",
-        "optionThree": "Disguises, deception and manipulation",
-        "optionFour": "A sword",
-        "answers": [
-            ["Magic potions and spells", 1],
-            ["My own brute force and strength", 3],
-            ["Disguises, deception and manipulation", 2],
-            ["A sword", 4]
-        ]
-    },
-    {
-        'question': "How would you work as a villain?",
-        "optionOne": "Completely alone",
-        "optionTwo": "With my sidekick, spirit animal",
-        "optionThree": "With a team of minions",
-        "optionFour": "Under the instructions of another villain",
-        "answers": [
-            ["Completely alone", 2],
-            ["With my sidekick, spirit animal", 3],
-            ["With a team of minions", 1],
-            ["Under the instructions of another villain", 4]
-        ]
-    },
-    {
-        'question': "Which personality trait applies to you most?",
-        "optionOne": "Aggressive",
-        "optionTwo": "Greedy",
-        "optionThree": "Narcissistic",
-        "optionFour": "Jealous",
-        "answers": [
-            ["Aggressive", 3],
-            ["Greedy", 1],
-            ["Narcissistic", 2],
-            ["Jealous", 4]
-        ]
-    }
-];
-
 let currentQuestionIndex = 0;
+document.getElementById("footer-area").classList.remove("hide");
 
 function updateProgress() {
     let questionIndex = currentQuestionIndex + 1;
@@ -170,14 +32,14 @@ takeQuizButton.addEventListener("click", getUserName);
 //let input = document.getElementById("name-submit");
 //let takeQuizButton = document.getElementById("take-quiz-btn");
 
-/*function initializeQuiz() {
+function initializeQuiz() {
     let startQuizButton = document.getElementById("take-quiz-btn");
     let retakeButton = document.getElementsByClassName("retake-button");
     startQuizButton.addEventListener("click", startQuiz);
     retakeButton.addEventListener("click", restartQuiz);
-}*/
+}
 
-// addEventListener('DOMContentLoaded', initializeQuiz);
+addEventListener('DOMContentLoaded', initializeQuiz);
 
 function startQuiz() {
 
